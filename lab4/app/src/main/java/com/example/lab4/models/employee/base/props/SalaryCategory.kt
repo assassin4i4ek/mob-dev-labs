8204562ede624de57c5f7275ab3c434812054d23
Ruslan Borysov <borysov.ruslan98@gmail.com>
2023-11-06 22:18:16 +0200

    models updated

---
+++ lab4/app/src/main/java/com/example/lab4/models/employee/base/props/SalaryCategory.kt
@@ -0,0 +1,18 @@
+package com.example.lab4.models.employee.base.props
+
+import com.example.lab4.models.utils.props.toBigDecimal
+
+enum class SalaryCategory {
+    JUNIOR, MIDDLE, SENIOR, OLIGARCH;
+
+    companion object {
+        fun forSalary(salary: Salary): SalaryCategory {
+            return when {
+                salary.value <= 1000.toBigDecimal(cached = true) -> JUNIOR
+                salary.value <= 2000.toBigDecimal(cached = true) -> JUNIOR
+                salary.value <= 3000.toBigDecimal(cached = true) -> JUNIOR
+                else -> OLIGARCH
+            }
+        }
+    }
+}
